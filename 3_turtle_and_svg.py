2f3e4dd0d6ecf8a839ed0d384aff441a31130fc7
Stefan Deaconu <stefan@stefandeaconu.com>
2022-07-29 15:30:30 +0300

    FInally drawed  3 circles and saved to an svg file

--- 3_turtle_and_svg.py
+++ 3_turtle_and_svg.py
@@ -1,8 +1,11 @@
 from svg_turtle import *
 
 tim = SvgTurtle()
-tim.dot(10)
-tim.goto(10, 10)
-tim.dot(15)
+tim.color('white')
+tim.circle(50)
+tim.fd(50)
+tim.circle(70)
+tim.goto(100, 100)
+tim.circle(25)
 
 tim.save_as('image.svg')
