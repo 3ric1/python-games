309897fe342355990699f0d81faf2a86f12f9be5
Stefan Deaconu <stefan@stefandeaconu.com>
2022-07-29 15:37:01 +0300

    Added a function used for drawing a square Used the function to draw 4 squares. The result is inside image.svg

--- 3_turtle_and_svg.py
+++ 3_turtle_and_svg.py
@@ -1,8 +1,20 @@
 from svg_turtle import *
 
+def square(alex: SvgTurtle, side: int):
+    x, y = alex.pos()
+    alex.goto(x, y + side)
+    alex.goto(x + side, y + side)
+    alex.goto(x + side, y)
+    alex.goto(x, y)
+
 tim = SvgTurtle()
-tim.dot(10)
-tim.goto(10, 10)
-tim.dot(15)
+tim.color('cyan')
+
+square(tim, 10)
+tim.fd(100)
+square(tim, 30)
+tim.goto(20, 32)
+square(tim, 50)
+square(tim, 27)
 
 tim.save_as('image.svg')
