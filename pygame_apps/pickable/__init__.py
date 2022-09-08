676396b7d7ed78d228670f780c3bbb2a427422ec
Stefan-Eduard Deaconu <stefan@stefandeaconu.com>
2022-09-08 15:45:09 +0300

    created classes used for easy continuous animation of any state from a sprite, such as the dog sit, run etc

---
+++ pygame_apps/pickable/__init__.py
@@ -0,0 +1,183 @@
+"""
+Objective: create a class named Pickable and a class named Positionable, this class will
+"""
+import time
+import math
+
+import pygame
+import easing_functions as ease  # TODO write inside the README.md
+from typing import Tuple, Union, List
+
+from pygame import KEYDOWN, K_ESCAPE
+from pygame.constants import QUIT
+
+from pygame_apps.pickable.sprite_animation import SpriteAnimation
+from pygame_apps.pickable.colors import Colors
+
+# trick #1
+Number = Union[int, float]
+Pair = Tuple[Number, Number]
+Triple = Tuple[Number, Number, Number]
+Quad = Tuple[Number, Number, Number, Number]
+
+
+# trick #2
+def Coords(x: int, y: int) -> pygame.rect.Rect:
+    return pygame.rect.Rect(x, y, 0, 0)
+
+
+class Positionable:
+
+    def __init__(self, pos: Pair):
+        self.x, self.y = pos
+
+
+class Pickable(Positionable):
+    def __init__(self, pos: Pair, texture: SpriteAnimation):
+        self.pos = pos
+        self.texture = texture
+
+
+class SimpleSprite:
+    def __init__(self, img_width: int, img_height: int, sprite: pygame.Surface, *,
+                 convert_to: Pair = None):
+        self.img_width = img_width
+        self.img_height = img_height
+        self.sprite = sprite
+        if convert_to is None:
+            self.out_width, self.out_height = img_width, img_height
+        self.out_width, self.out_height = convert_to
+
+    def get(self, row: int, col: int) -> pygame.Surface:
+        text = pygame.surface.Surface((self.img_width, self.img_height), flags=pygame.SRCALPHA)
+        print((
+            col * self.img_width,
+            row * self.img_height,
+            self.img_width,
+            self.img_height
+        ))
+        text.blit(
+            self.sprite,
+            Coords(0, 0),
+            area=pygame.rect.Rect(
+                col * self.img_width,
+                row * self.img_height,
+                self.img_width,
+                self.img_height
+            )
+        )
+        return pygame.transform.scale(text, (self.out_width, self.out_height))
+
+
+# There a number (so multiple) objects present in games (items, textures, walls, traps, NPCs, enemies)
+#  which have diverse states (standing, sitting, running, attacking, trap getting triggered) which have different animations.
+
+class Stateful:
+
+    def __init__(self, states: List[str], *, initial_state: Union[int, str] = 0):
+        self.states = states
+
+        self.state_index = 0
+        self.update_state(initial_state)
+
+    def update_state(self, state: Union[int, str]):
+        if isinstance(state, int) and 0 <= state < len(self.states):
+            self.state_index = state
+        else:
+            # look up by name
+            i = self.states.index(state)
+            # if not found, use first animation:
+            if i == -1:
+                i = 0
+            self.state_index = i
+
+
+class StatefulEntity(Positionable, Stateful):
+    def __init__(self,
+                 pos: Pair,
+                 states: List[str], nominal_anim_fps: int,
+                 sprite: pygame.Surface, size: Pair, frame_count_dict: dict,
+                 **kwargs):
+        # pass fields for Positionable
+        Positionable.__init__(self, pos)
+        # pass fields for Stateful, it it exists
+        Stateful.__init__(self, states, initial_state=kwargs.get('initial_state', 0))
+
+        # create the animation sprite
+        self.anim_sprite = SpriteAnimation(sprite, size, states, frame_count_dict)
+        self.nominal_anim_fps = nominal_anim_fps
+
+        # for animating continuously
+        self.frame_index = 0
+        # TODO add a continuous timer
+        self.t = time.time()
+
+    @property
+    def curr_texture(self) -> pygame.Surface:
+        # how many frames, at the nominal_anim_fps, have passed since self.t ?
+        dt = time.time() - self.t  # !!! seconds
+        frames_since = math.floor(dt * self.nominal_anim_fps)
+
+        print(dt, frames_since)
+        curr_state = self.states[self.state_index]
+        self.frame_index = frames_since % self.anim_sprite.frame_count(
+            curr_state
+        )  # TODO may divide by 0, if the state name is wrong
+
+        return self.anim_sprite.at(self.states[self.state_index], self.frame_index)
+
+
+if __name__ == '__main__':
+    pygame.init()
+    screen = pygame.display.set_mode((600, 200), flags=pygame.SRCALPHA)
+
+    # create textures and other actors/entities..
+    text_ui_icons = pygame.image.load('../maze/assets/ui_icons.png').convert_alpha()
+    ui_sprite = SimpleSprite(500, 500, text_ui_icons, convert_to=(50, 50))
+
+    dog_sprite = pygame.image.load('../maze/assets/Dog_medium.png').convert_alpha()
+    dog_entity = StatefulEntity(
+        (0, 0),
+        ['Bark', 'Walk', 'Run', 'Sit Transition', 'Idle Sit', 'Idle Stand'],
+        5,  # so the image changes 10 times per second, no matter the state
+        #       maybe we should set it for each frame, optionally?
+        dog_sprite, (60, 38), {'Bark': 4,
+                               'Walk': 6,
+                               'Run': 5,
+                               'Sit Transition': 3,
+                               'Idle Sit': 4,
+                               'Idle Stand': 4, }
+    )
+    dog_entity.update_state(4)
+
+    clock = pygame.time.Clock()
+    while True:
+        # Look at every event in the queue
+        for event in pygame.event.get():
+            # Did the user hit a key?
+            if event.type == KEYDOWN:
+                # Was it the Escape key? If so, stop the loop.
+                if event.key == K_ESCAPE:
+                    quit()
+
+            # Did the user click the window close button? If so, stop the loop.
+            elif event.type == QUIT:
+                quit()
+
+        dtime = clock.tick(5)
+
+        screen.fill(Colors.PINE)
+
+        # # the new frame:
+        # coin = ui_sprite.get(0, 0)
+        # coin2 = ui_sprite.get(0, 1)
+        #
+        # # screen.blit(coin, Coords(0, 0))
+        # screen.blit(coin2, Coords(0, 0))
+        # pygame.draw.circle(screen, (255, 0, 0), (100, 100), 15)
+
+        dog = dog_entity.curr_texture
+        dog = pygame.transform.flip(dog, flip_x=True, flip_y=False)
+        screen.blit(dog, Coords(0, 0))
+
+        pygame.display.flip()
